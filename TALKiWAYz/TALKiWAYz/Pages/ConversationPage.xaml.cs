4467288a6d25d5da48ad8dd4f930104eeaa35f9b
seanmcneilly2 <33751736+seanmcneilly2@users.noreply.github.com>
2018-03-11 02:08:50 +0000

    Can now record audio from microphone

--- TALKiWAYz/TALKiWAYz/Pages/ConversationPage.xaml.cs
+++ TALKiWAYz/TALKiWAYz/Pages/ConversationPage.xaml.cs
@@ -17,7 +17,8 @@ using Newtonsoft.Json;
 
 using TALKiWAYz.Models;
 using TALKiWAYz.Utils;
-using UWPTextToSpeech;
+using UWPSpeechToText;
+using Button = Windows.UI.Xaml.Controls.Button;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -29,13 +30,18 @@ namespace TALKiWAYz.Pages
     public sealed partial class ConversationPage : Page
     {
         private CortanaTalk Cortana = new CortanaTalk();
+        private STT SpeechToText = new STT();
+
         private ConversationDetails ConversationDetails;
+        private int Score = 0;
 
         private int currentPage = 0;
 
         public ConversationPage()
         {
             this.InitializeComponent();
+
+            ScoreText.Text = "Score: 0";
 
             ConversationDetails =
                 JsonConvert.DeserializeObject<ConversationDetails>(File.ReadAllText("./Assets/Conversation.json"));
@@ -52,21 +58,35 @@ namespace TALKiWAYz.Pages
             QuestionText.Text = statement.SpeechText;
             for (var i = 0; i < statement.Responses.Count; i++)
             {
-                var responseTextBlock = new TextBlock();
-                responseTextBlock.Name = $"response{i}";
+                var responseTextBlock = new Button();
+                responseTextBlock.Name = $"response_{i}";
                 responseTextBlock.FontSize = 20d;
-                responseTextBlock.Text = statement.Responses[i].Text;
+                responseTextBlock.Content = statement.Responses[i].Text;
+                responseTextBlock.Click += onResponseClick;
 
                 Canvas.SetLeft(responseTextBlock, 500);
-                Canvas.SetTop(responseTextBlock, 150 + (i * 20));
+                Canvas.SetTop(responseTextBlock, 150 + (i * 50));
                 ResponseCanvas.Children.Add(responseTextBlock);
             }
 
             var talkMethod = await Cortana.Talk(QuestionText.Text);
+
+            var detectedText = await SpeechToText.Listen();
         }
 
-        private void NextQuestion_OnClick(object sender, RoutedEventArgs e)
+        private void onResponseClick(object sender, RoutedEventArgs e)
         {
+            var button = (Button) sender;
+            var responseId = int.Parse(button.Name.Split('_')[1]);
+
+            var statement = ConversationDetails.Conversations[0].Statements[currentPage];
+            if (statement.Responses[responseId].Correct)
+            {
+                Score++;
+            }
+
+            ScoreText.Text = $"Score: {Score}";
+
             currentPage++;
             DrawPage(currentPage);
         }
